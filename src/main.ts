525a122f5fdeba19734fc5b608a3317ef7d8cfaf
Marj4n <nivkaiser@gmail.com>
2025-11-10 03:45:53 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('STK Backend API')
     .setDescription('API documentation for the STK backend.')
